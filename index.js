336723788e23115ac46f97661fa3e6f5575432e3
pkwarlock <pkwarlock.cs49@gmail.com>
2023-02-14 11:20:35 +0700

    Add histogram metric

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const app = fastify({
 });
 const metricsPlugin = require('fastify-metrics');
 
-app.register(metricsPlugin, { endpoint: '/metrics' });
+app.register(metricsPlugin, {endpoint: '/metrics', 
+  routeMetrics: {
+    overrides: {
+      histogram: {
+        name: 'my_custom_http_request_duration_seconds',
+        buckets: [0.1, 0.5, 1, 3, 5],
+      },
+      summary: {
+        help: 'custom request duration in seconds summary help',
+        labelNames: ['status_code', 'method', 'route'],
+        percentiles: [0.5, 0.75, 0.9, 0.95, 0.99],
+      },
+    }
+  }
+});
 
 
 app.get('/', function (request, reply) {
